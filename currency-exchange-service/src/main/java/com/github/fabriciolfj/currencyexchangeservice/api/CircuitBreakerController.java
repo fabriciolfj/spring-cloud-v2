3cb6c310f6afdbc5fc25b6acfed23a4622225c42
fabricio <fabricio.jacob>
2021-02-18 09:13:24 -0300

    ad

---
+++ currency-exchange-service/src/main/java/com/github/fabriciolfj/currencyexchangeservice/api/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package com.github.fabriciolfj.currencyexchangeservice.api;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardCodeResponse")
+    public String sampleApi() {
+        logger.info("Sample api call received");
+        var restTemplate = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+
+        return restTemplate.getBody();
+    }
+
+    public String hardCodeResponse(Exception ex) {
+        return "fallback response";
+    }
+}
---
+++ naming-server/src/test/java/com/github/fabriciolfj/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
