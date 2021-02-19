6cfedf2f2778e6aec26cc5decae8caabd8f8b2e1
fabricio <fabricio.jacob>
2021-02-18 21:35:32 -0300

    add

--- currency-exchange-service/src/main/java/com/github/fabriciolfj/currencyexchangeservice/api/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/github/fabriciolfj/currencyexchangeservice/api/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.github.fabriciolfj.currencyexchangeservice.api;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +16,10 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardCodeResponse")
+    //@Retry(name = "sample-api", fallbackMethod = "hardCodeResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardCodeResponse")
+    //@RateLimiter(name = "sample-api")
+    @Bulkhead(name = "sample-api")
     public String sampleApi() {
         logger.info("Sample api call received");
         var restTemplate = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
